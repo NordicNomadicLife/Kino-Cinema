78efda08ed3b1c1d8b3452775f0e639da63006fa
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-01-17 14:42:15 +0100

    started a simple server

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 import express from 'express';
 
 const app = express();
-const PORT = 5080;
 
-// Middleware för att serva statiska filer
-app.use(express.static('public'));
-
-// Starta servern
-app.listen(PORT, () => {
-  console.log(`Servern körs på http://localhost:${PORT}`);
+app.get('/', (request, response) => {
+    response.send('hello, world');
 });
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Kino', welcomeMessage: 'Välkommen till Kino!' });
-  });
-  
+app.listen(5080, () => {
+    console.log('Server is running on http://localhost:5080');
+});
