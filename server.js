893ded409de2ea9e13c015e7a4cb957d2200d64e
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-01-23 14:34:28 +0100

    made so i can start test whitout starting a server

--- server.js
+++ server.js
@@ -1,51 +1,14 @@
 import express from 'express';
 import path from 'path';
 import { engine } from 'express-handlebars';
-import { loadMovie, loadMovies } from "./importMovies.js";
+import initApp from './app.js';
+import { loadMovie, loadMovies } from './importMovies.js';
 
-const app = express();
-const PORT = 5080;
+const api={
+  loadMovie,
+  loadMovies,
+};
 
-// Ad Handlebars as templateengine
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./templates");
-
-app.use("/static", express.static("./static"));
-
-
-// Route for homepage
-app.get('/', (req, res) => {
-    res.render('index', 
-      { title: 'Kino Cinema', 
-        welcomeMessage: 'Välkommen till Kino!' });
-});
-
-// Route for moviespage
-app.get('/moviesPage', (req, res) => {
-    res.render('moviesPage', 
-      { title: 'Film sida', });
-});
-
-/* // Route for Allmovies
-app.get('/allMovies', (req, res) => {
-  res.render('allMovies', 
-    { title: 'Film sida', });
-}); */
-
-
-
-app.get("/allMovies", async (req, res) => {
-    const movies = await loadMovies();
-    res.render("allMovies",{ movies });
-  });
-  
-  app.get("/movies/:movieId", async (req, res) => {
-    const movie = await loadMovie(req.params.movieId);
-    res.render("movie", { movie });
-  });
-
+const app = initApp(api);
 // Start server
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+app.listen(5080);
