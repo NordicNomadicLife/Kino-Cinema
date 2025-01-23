9a76eb0283fd72375c6f45a944599bfd1104db7d
NordicNomadicLife <Pelle.nilsson.93@hotmail.com>
2025-01-23 11:34:54 +0100

    uppdated with correct links and working path

--- importMovies.js
+++ importMovies.js
@@ -1,15 +1,17 @@
 import fetch from "node-fetch";
 
-const API_BASE = 'https://plankton-app-xhkom.ondigitalocean.app/api/movies';
-const API_ID = 'https://plankton-app-xhkom.ondigitalocean.app/api/movies/<id>';
+const ApiAllMovies = 'https://plankton-app-xhkom.ondigitalocean.app/api/movies';
+const ApiSingleMovie = 'https://plankton-app-xhkom.ondigitalocean.app/api/movies';
+
 export async function loadMovies() {
-  const res = await fetch(API_BASE + '/movies');
-  const payload = await res.json();
-  return payload.data;
+
+    const res = await fetch(ApiAllMovies);
+    const payload = await res.json();
+    return payload.data;
 }
 
 export async function loadMovie(id) {
-  const res = await fetch(API_BASE + '/movies/' + id);
-  const payload = await res.json();
-  return payload.data;
+    const res = await fetch(`${ApiSingleMovie}/${id}`);
+    const payload = await res.json();
+    return payload.data;
 }
